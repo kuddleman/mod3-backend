2edee78aee734a2b011b43b8c72e0a551b8098b7
github username <ingrey1@gmail.com>
2020-02-10 11:17:04 -0800

    added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create, :update, :destroy] do
+        resources :playlists, only: [:show, :index, :create, :update, :destroy] do 
+
+
+          resources :songs, only: [:index, :show]
+
+        end 
+      end 
+    end
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
