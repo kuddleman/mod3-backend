b7026c7fbd9f36de025e53bc114462ed138a0db9
github username <ingrey1@gmail.com>
2020-02-10 11:27:00 -0800

    added strong params to User model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+    def login
+    end 
+    
+    def create
+    end 
+
+    def show 
+
+    end
+    
+    def update
+
+    end 
+
+    def destroy
+        
+    end 
+
+    private 
+
+    def strong_params
+       params.require(:user_info).permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
+    end 
 end
