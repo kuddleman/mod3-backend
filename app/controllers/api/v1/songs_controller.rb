83f0f2077fe1ffc8c5a6c923215d46926da41f5f
github username <ingrey1@gmail.com>
2020-02-11 15:28:40 -0800

    added destroy method to song controller

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::SongsController < ApplicationController
+  def destroy
+    if authorized
+      song = Song.find(params[:id])
+      user = User.find(params[:user_id])
+      if song and song.destroy
+        render json: createUserData(user, false)
+      else
+        render json: { error: "the song couldnt be deleted" }
+      end
+    end
+  end
+
   def create
     #byebug
-    user = User.find(params[:user_id])
+
     #byebug
     if authorized
       # create the song
-
+      user = User.find(params[:user_id])
       song = Song.new(strong_params)
       if song.save!
         # add song to playlist
