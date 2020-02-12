94b7dd272a47f0accd412439f7f85e727783a03a
github username <ingrey1@gmail.com>
2020-02-12 10:36:00 -0800

    added index action playlist controller

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,10 +1,31 @@
 class Api::V1::PlaylistsController < ApplicationController
     def index
       if authorized
-       
+        render json: createUserData(User.find(params[:user_id]), false) 
       else
-        render json: { errors: "You're not authorized" }.to_json
+        render json: { error: "You're not authorized" }.to_json
       end
     end
+
+    def create
+      
+      if authorized
+        user = User.find(params[:user_id])
+        new_playlist = Playlist.new()
+      else     
+         render json: {error: "you're not authorized" }
+      end
+      
+     
+      
+    end 
+
+    private 
+
+    def strong_params
+       params.require(:name)
+    end 
+
+
   end
   
