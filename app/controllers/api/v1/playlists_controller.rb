e11e92a867aa709314e1a8579844841c2532dcd1
github username <ingrey1@gmail.com>
2020-02-14 09:21:07 -0800

    done

--- app/controllers/api/v1/playlists_controller.rb
+++ app/controllers/api/v1/playlists_controller.rb
@@ -1,43 +1,43 @@
 class Api::V1::PlaylistsController < ApplicationController
-    def index
+  def index
+    token = authenticated
 
-      token = authenticated
-       
-      if token
-        render json: createUserData(User.find(token), false, false) 
-      else
-        render json: { error: "You're not authorized" }.to_json
-      end
-    end   
-
-    def create
-      
-      if authorized
-        user = User.find(params[:user_id])
-        new_playlist = Playlist.new(strong_params)
-        user.playlists << new_playlist
-        render json: createUserData(user, false, false)
-      else     
-         render json: {error: "you're not authorized" }
-      end
-    end 
-
-    def destroy
-       if authorized
-         user = User.find(params[:user_id])
-         Playlist.find(params[:playlist_id]).destroy
-         render json: createUserData(user, false, false) 
-       else
-         render json: {error: "you're not authorized"}
-       end 
-    end 
-
-    private 
-
-    def strong_params
-       params.require(:name)
-    end 
-
-
+    if token
+      render json: createUserData(User.find(token), false, false)
+    else
+      render json: { error: "You're not authorized" }.to_json
+    end
   end
-  
+
+  def create
+    if authorized
+      #byebug
+      user = User.find(params[:user_id])
+      #byebug
+      new_playlist = Playlist.new(title: strong_params)
+
+      user.playlists << new_playlist
+      #byebug
+      render json: createUserData(user, false, false)
+    else
+      render json: { error: "you're not authorized" }
+    end
+  end
+
+  def destroy
+    if authorized
+      #byebug
+      user = User.find(params[:user_id])
+      Playlist.find(params[:id]).destroy
+      render json: createUserData(user, false, false)
+    else
+      render json: { error: "you're not authorized" }
+    end
+  end
+
+  private
+
+  def strong_params
+    params.require(:title)
+  end
+end
